638f113f8c7b63a9fb53a3a4b909068f72ffc7e7
aseo <aseo@yahoo-corp.jp>
2019-06-19 23:48:08 +0900

    feat: GTFS形式のZIPファイルを作成するコマンドを用意

---
+++ make_zip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ファイルのコピー
+cp ./static/* ./output
+# カレントディレクトリの変更
+cd ./output
+# ファイルの拡張子一括変更
+for filename in *.csv; do mv $filename ${filename%.csv}.txt; done
+# zipファイルの作成
+zip -r `date "+%Y%m%d%H%M%S"`.zip .
+# テキストファイルの一括削除
+rm *.txt
